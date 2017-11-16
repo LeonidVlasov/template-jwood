598b225996e904cddd06b593e4fc3caa450686b8
LeonidVlasov <leonid.vlasov@gmail.com>
2017-11-16 15:03:28 +0200

    jquery added to ham-menu

--- js/common.js
+++ js/common.js
@@ -1,1 +1,14 @@
 'use strict';
+$(document).ready(function (){
+    
+    $(".toggle").click(function () {
+        $(".down").toggleClass('downstart');
+        $(".fade").toggleClass("fadestart");
+        $(".up").toggleClass("upstart");
+    });
+
+    $(".toggle").click(function () {
+        $(".header__menu").toggleClass('header__menu--active');
+        $(".logobox").toggleClass('logobox--disabled');
+    });
+});
