e8d7b01e946964104eb847380a35437c029d5c87
LeonidVlasov <leonid.vlasov@gmail.com>
2017-11-16 16:16:09 +0200

    smooth scroll added, hamburger manu finished

--- js/common.js
+++ js/common.js
@@ -1,14 +1,22 @@
 'use strict';
 $(document).ready(function (){
+
+    $("a").click(function () {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 1000);
+        return false;
+    });
     
     $(".toggle").click(function () {
         $(".down").toggleClass('downstart');
         $(".fade").toggleClass("fadestart");
         $(".up").toggleClass("upstart");
-    });
-
-    $(".toggle").click(function () {
         $(".header__menu").toggleClass('header__menu--active');
         $(".logobox").toggleClass('logobox--disabled');
+    });
+
+    $(".item__link").click(function () {
+        $(".toggle").click();
     });
 });
